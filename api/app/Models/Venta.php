2cabc86a77110124ff43e34acc5352a056be98bb
CarlosIrigoyen <cyrigoyen969@gmail.com>
2025-06-03 19:39:34 -0300

    agregado el controlador de venta y los modelos nesecarios para la misma

---
+++ api/app/Models/Venta.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    protected $fillable = ['cliente_id', 'total'];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function factura()
+    {
+        return $this->hasOne(Factura::class);
+    }
+
+    public function detalles()
+    {
+        return $this->hasMany(DetalleVenta::class);
+    }
+}
