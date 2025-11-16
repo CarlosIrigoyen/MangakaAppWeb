c828acd93d0fdee78e13d36a149c3c76907860f5
Irigoyen Carlos Damian <142269298+CarlosIrigoyen@users.noreply.github.com>
2025-11-15 22:13:46 -0300

    Refactor logout link in navbar
    Updated logout link styling and structure.

--- api/resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
+++ api/resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
@@ -7,10 +7,14 @@
 @endif
 
 <li class="nav-item">
-    <a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        <i class="fa fa-fw fa-power-off text-red"></i>
+    <a class="nav-link logout-link"
+       href="#"
+       style="color: rgba(0,0,0,1) !important; display:flex; align-items:center;"
+       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <i class="fa fa-fw fa-power-off" aria-hidden="true" style="color: rgba(0,0,0,1) !important; margin-right: .4rem;"></i>
         {{ __('adminlte::adminlte.log_out') }}
     </a>
+
     <form id="logout-form" action="{{ $logout_url }}" method="POST" style="display: none;">
         @if(config('adminlte.logout_method'))
             {{ method_field(config('adminlte.logout_method')) }}
