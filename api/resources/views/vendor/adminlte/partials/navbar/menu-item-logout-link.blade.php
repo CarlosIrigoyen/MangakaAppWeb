8cc0c8409b2ecf614637428a6925975eeff32e27
Irigoyen Carlos Damian <142269298+CarlosIrigoyen@users.noreply.github.com>
2025-11-15 22:16:20 -0300

    Change logout link to use button for submission

--- api/resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
+++ api/resources/views/vendor/adminlte/partials/navbar/menu-item-logout-link.blade.php
@@ -7,18 +7,18 @@
 @endif
 
 <li class="nav-item">
-    <a class="nav-link logout-link"
-       href="#"
-       style="color: rgba(0,0,0,1) !important; display:flex; align-items:center;"
-       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-        <i class="fa fa-fw fa-power-off" aria-hidden="true" style="color: rgba(0,0,0,1) !important; margin-right: .4rem;"></i>
-        {{ __('adminlte::adminlte.log_out') }}
-    </a>
-
-    <form id="logout-form" action="{{ $logout_url }}" method="POST" style="display: none;">
+    <form id="logout-form" action="{{ $logout_url }}" method="POST" style="display: inline;">
         @if(config('adminlte.logout_method'))
             {{ method_field(config('adminlte.logout_method')) }}
         @endif
         {{ csrf_field() }}
+
+        <button type="submit"
+                class="nav-link btn btn-link p-0 logout-button"
+                style="color: rgba(0,0,0,1); display:flex; align-items:center; text-decoration:none;">
+            <i class="fa fa-fw fa-power-off" aria-hidden="true" style="color: rgba(0,0,0,1); margin-right: .4rem;"></i>
+            <span>{{ __('adminlte::adminlte.log_out') }}</span>
+        </button>
     </form>
 </li>
+
